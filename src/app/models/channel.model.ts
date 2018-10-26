66c765f8a615421c7cca2c495afb318d7ff16fa3
Brian Nelson <nelsonsbrian@gmail.com>
2018-10-26 15:33:01 -0700

    add addtoplaylist and removefromplaylist functions to channel class

--- src/app/models/channel.model.ts
+++ src/app/models/channel.model.ts
@@ -15,26 +15,65 @@ export class Channel {
     public history: Video[] = [];
     public watchLater: Playlist[] = [];
     public videosLiked: Video[] = [];
+    public loggedIn: boolean = false;
+    static master: Channel = null;
 
 
     constructor(public name?: string, public img?: string, public description?: string) {};
 
-    isDuplicateVideo(videoId: number) {
+    isDuplicateUpload(videoId: number) {
         for (let i = 0; i < this.videos.length; i ++) {
             if (this.videos[i].id === videoId) {
                 return true;
             }
         }
         return false;
+    }
+
+    isDuplicateOnPlaylist(videoId: number, playList: Playlist) {
+        for (let i = 0; i < playList.list.length; i ++) {
+            if (playList.list[i].id === videoId) {
+                return true;
+            }
+        }
+        return false;
     }    
 
-    getVideo(videoId: number) {
-        
+    addToPlayList(video: Video, playList: Playlist) {
+        if (this.isDuplicateOnPlaylist(video.id, playList)) {
+            playList.list.push(video);
+        }
     }
 
-    watchVideo(videoid: number) {
-        this.videos.push()
+    removeFromPlayList(video: Video, playList: Playlist) {
+        if (!(this.isDuplicateOnPlaylist(video.id, playList))) {
+            let index = null;
+            for (let i = 0; i < playList.list.length; i++) {
+                if (playList.list[i].id = video.id) {
+                    index = i;
+                }
+            }
+            playList.list.splice(index,1);
+        }
+    }    
+
+
+    watchVideo(video: Video) {
+        video.watchedVideo();
+        this.history.push(video);
     }
+
+    logOn() {
+        this.loggedIn = true;
+        Channel.master = this;
+    }
+
+    logOff() {
+        this.loggedIn = false;
+        Channel.master = null;
+    }
+
+
 }
 
 
