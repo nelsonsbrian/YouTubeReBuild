27b72481982a370ebd75b14b39cbcd715930cad7
Brian Nelson <nelsonsbrian@gmail.com>
2018-11-02 08:23:56 -0700

    link api to api query

--- src/app/youtube-api.service.ts
+++ src/app/youtube-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { youTubeApi } from './api-keys';
 
 @Injectable()
 export class YoutubeApiService {
@@ -8,10 +9,10 @@ export class YoutubeApiService {
   constructor(private http: Http) { }
 
   getAllCategories() {
-    return this.http.get(`https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=AIzaSyBwtlSaWul_MvmfaqxE_kbNDIKAxIH8n-g`);
+    return this.http.get(`https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=${youTubeApi}`);
   }
 
   getVideosByCategory(catId: string) {
-    return this.http.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&videoCategoryId=${catId}&key=AIzaSyBwtlSaWul_MvmfaqxE_kbNDIKAxIH8n-g`);
+    return this.http.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&videoCategoryId=${catId}&key=${youTubeApi}`);
   }
 }
